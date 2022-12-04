cf5cb921116499656842c8ff039c5da2bb665de9
JeonghyeonK <6843wjdgus@naver.com>
2022-12-05 05:20:54 +0900

    초기 메뉴 출력 구현

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,131 +6,131 @@ public class Main
 {
 
 
+
     public static void main(String args[])
     {
+        Connection con=null;
+        PreparedStatement prep_stmt=null;
+        Statement stmt=null;
 
-        while(true) {
+        try{
+            // madang 데이터베이스와 연결
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con=DriverManager.getConnection(
+                    "jdbc:mysql://192.168.16.3:4567/delivery",
+                    "jhkim","qaz123qaz");
+            stmt=con.createStatement();
 
-            // 어떤 작업을 수행할지 입력받음
-            System.out.println("작업을 입력하세요. \n1. 데이터 삽입 \n2. 데이터 삭제 \n3. 데이터 검색 \n4. 전체 데이터 출력 \n5. 종료");
-            Scanner s = new Scanner(System.in);
-            int work = s.nextInt();
 
-            // 입력받은 작업들을 수행
-            if(work==1){
+            while(true) {
 
-                try{
-                    // madang 데이터베이스와 연결
-                    Class.forName("com.mysql.cj.jdbc.Driver");
-                    Connection con=DriverManager.getConnection(
-                            "jdbc:mysql://192.168.16.3:4567/madang",
-                            "jhkim","qaz123qaz");
 
-                    // 삽입할 데이터 정보 입력받기
-                    System.out.print("책 이름을 입력하세요 : ");
-                    Scanner sc1 = new Scanner(System.in);
-                    String bookname = sc1.nextLine();
-                    System.out.print("출판사를 입력하세요 : ");
-                    Scanner sc2 = new Scanner(System.in);
-                    String publisher = sc2.nextLine();
 
-                    // 쿼리문 작성
-                    PreparedStatement stmt=con.prepareStatement(
-                            "INSERT INTO Book(bookname, publisher) VALUES ('"+bookname+"', '"+publisher+"');");
+                // 어떤 작업을 수행할지 입력받음
+                System.out.print("\n\n\n*** 배달의 종족 *** \n무엇을 하시겠습니까? \n\n1. 로그인 \n2. 회원 가입 \n3. 종료 \n\n입력 : ");
+                Scanner s = new Scanner(System.in);
+                int work = s.nextInt();
 
-                    // 쿼리문 실행
-                    stmt.execute();
-                    con.close();
+                // 입력받은 작업들을 수행
+                if(work==1){
 
-                    // 성공시 결과 출력, 실패시 오류 출력
-                    System.out.println("입력되었습니다.\n");
-                }catch(Exception e){ System.out.println(e);}
+                    try{
 
-            } // 데이터 삽입
+                        // 삽입할 데이터 정보 입력받기
+                        System.out.print("책 이름을 입력하세요 : ");
+                        Scanner sc1 = new Scanner(System.in);
+                        String bookname = sc1.nextLine();
+                        System.out.print("출판사를 입력하세요 : ");
+                        Scanner sc2 = new Scanner(System.in);
+                        String publisher = sc2.nextLine();
 
-            else if(work==2){
+                        // 쿼리문 작성
+                        prep_stmt=con.prepareStatement(
+                                "INSERT INTO Book(bookname, publisher) VALUES ('"+bookname+"', '"+publisher+"');");
 
-                try{
-                    // madang 데이터베이스와 연결
-                    Class.forName("com.mysql.cj.jdbc.Driver");
-                    Connection con=DriverManager.getConnection(
-                            "jdbc:mysql://192.168.16.3:4567/madang",
-                            "jhkim","qaz123qaz");
+                        // 쿼리문 실행
+                        prep_stmt.execute();
 
-                    // 삭제할 데이터 정보 입력받기
-                    System.out.print("책 이름을 입력하세요 : ");
-                    Scanner sc1 = new Scanner(System.in);
-                    String bookname = sc1.nextLine();
+                        // 성공시 결과 출력, 실패시 오류 출력
+                        System.out.println("입력되었습니다.\n");
+                    }catch(Exception e){ System.out.println(e);}
 
-                    // 쿼리문 작성
-                    PreparedStatement stmt=con.prepareStatement("DELETE FROM Book WHERE bookname = '"+bookname+"';");
+                } // 데이터 삽입
 
-                    // 쿼리문 실행
-                    stmt.execute();
-                    con.close();
+                else if(work==2){
 
-                    // 성공시 결과 출력, 실패시 오류 출력
-                    System.out.println("삭제되었습니다.\n");
-                }catch(Exception e){ System.out.println(e);}
-            } // 데이터 삭제
+                    try{
 
-            else if(work==3){
-                try{
-                    // madang 데이터베이스와 연결
-                    Class.forName("com.mysql.cj.jdbc.Driver");
-                    Connection con=DriverManager.getConnection(
-                            "jdbc:mysql://192.168.16.3:4567/madang",
-                            "jhkim","qaz123qaz");
-                    Statement stmt=con.createStatement();
+                        // 삭제할 데이터 정보 입력받기
+                        System.out.print("책 이름을 입력하세요 : ");
+                        Scanner sc1 = new Scanner(System.in);
+                        String bookname = sc1.nextLine();
 
-                    // 검색할 키워드 입력받기
-                    System.out.print("검색어를 입력하세요 : ");
-                    Scanner sc = new Scanner(System.in);
-                    String keyword = sc.nextLine();
+                        // 쿼리문 작성
+                        prep_stmt=con.prepareStatement("DELETE FROM Book WHERE bookname = '"+bookname+"';");
 
-                    // 쿼리문 작성 및 실행
-                    ResultSet rs=stmt.executeQuery("SELECT * FROM Book WHERE bookname LIKE '%" + keyword + "%';");
+                        // 쿼리문 실행
+                        prep_stmt.execute();
 
-                    // 성공시 검색 결과 출력, 실패시 오류 출력
-                    System.out.println("\n검색 결과 : ");
-                    while(rs.next())
-                        System.out.println(rs.getInt(1)+" "+rs.getString(2)+
-                                " "+rs.getString(3));
-                    System.out.println();
-                    con.close();
-                }catch(Exception e){ System.out.println(e);}
+                        // 성공시 결과 출력, 실패시 오류 출력
+                        System.out.println("삭제되었습니다.\n");
+                    }catch(Exception e){ System.out.println(e);}
+                } // 데이터 삭제
 
-            } // 데이터 검색
+                else if(work==3){
+                    try{
 
-            else if(work==4){
+                        // 검색할 키워드 입력받기
+                        System.out.print("검색어를 입력하세요 : ");
+                        Scanner sc = new Scanner(System.in);
+                        String keyword = sc.nextLine();
 
-                try{
-                    // madang 데이터베이스와 연결
-                    Class.forName("com.mysql.cj.jdbc.Driver");
-                    Connection con=DriverManager.getConnection(
-                            "jdbc:mysql://192.168.16.3:4567/madang",
-                            "jhkim","qaz123qaz");
+                        // 쿼리문 작성 및 실행
+                        ResultSet rs=stmt.executeQuery("SELECT * FROM Book WHERE bookname LIKE '%" + keyword + "%';");
 
-                    // 쿼리문 작성 및 실행
-                    Statement stmt=con.createStatement();
-                    ResultSet rs=stmt.executeQuery("SELECT * FROM Book");
+                        // 성공시 검색 결과 출력, 실패시 오류 출력
+                        System.out.println("\n검색 결과 : ");
+                        while(rs.next())
+                            System.out.println(rs.getInt(1)+" "+rs.getString(2)+
+                                    " "+rs.getString(3));
+                        System.out.println();
+                    }catch(Exception e){ System.out.println(e);}
 
-                    // 성공시 결과 출력, 실패시 오류 출력
-                    System.out.println();
-                    while(rs.next())
-                        System.out.println(rs.getInt(1)+" "+rs.getString(2)+
-                                " "+rs.getString(3));
-                    System.out.println();
-                    con.close();
-                }catch(Exception e){ System.out.println(e);}
+                } // 데이터 검색
 
-            } // 전체 데이터 출력
+                else if(work==4){
 
-            else if(work==5) break; //종료
+                    try{
 
-            else System.out.println("잘못된 입력입니다."); // 1~5 외 입력시 안내문구 출력
-            //test
-        }
+                        // 쿼리문 작성 및 실행
+                        ResultSet rs=stmt.executeQuery("SELECT * FROM Menu");
+
+                        // 성공시 결과 출력, 실패시 오류 출력
+                        System.out.println();
+                        while(rs.next())
+                            System.out.println(rs.getInt(1)+" "+rs.getString(2)+
+                                    " "+rs.getString(3));
+                        System.out.println();
+                    }catch(Exception e){ System.out.println(e);}
+
+                } // 전체 데이터 출력
+
+                else if(work==5) break; //종료
+
+                else System.out.println("잘못된 입력입니다."); // 1~5 외 입력시 안내문구 출력
+                //test
+            }
+
+
+
+
+            con.close();
+
+            // 성공시 결과 출력, 실패시 오류 출력
+            System.out.println("입력되었습니다.\n");
+        }catch(Exception e){ System.out.println(e);}
+
+
 
     }
 }
