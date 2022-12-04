2c4a578730a8072c73922c5510df3990a3c980da
JeonghyeonK <6843wjdgus@naver.com>
2022-12-05 08:02:47 +0900

    UserPage에서 Store 목록, Store별로 세부 정보, 메뉴 목록, 리뷰 목록 출력, 로그아웃 기능 구현

--- src/main/java/org/example/OrderAndReview.java
+++ src/main/java/org/example/OrderAndReview.java
@@ -6,15 +6,17 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Scanner;
 
-public class UserPage {
+public class OrderAndReview {
 
     private int userId;
+    private int storeId;
     private Connection con;
     private PreparedStatement prep_stmt = null;
     private Statement stmt = null;
 
-    public UserPage(int userId, Connection con) {
+    public OrderAndReview(int userId, int storeId, Connection con) {
         this.userId = userId;
+        this.storeId = storeId;
         this.con = con;
 
 
@@ -53,30 +55,34 @@ public class UserPage {
                     // 유저, 사장님, 라이더 중 선택
                     System.out.print("\n가게 아이디를 입력하세요. \n\n입력 : ");
                     Scanner st= new Scanner(System.in);
-                    int storeId = st.nextInt();
+                    int store_id = st.nextInt();
 
 
                     // 쿼리문 작성 및 실행
                     stmt = con.createStatement();
-                    ResultSet rs = stmt.executeQuery("SELECT * FROM Store where id = " + storeId + ";");
+                    ResultSet rs = stmt.executeQuery("SELECT * FROM Store where id = " + store_id + ";");
                     // 성공시 검색 결과 출력, 실패시 오류 출력
                     System.out.printf("\n%-4s %-10s %-5s %-8s %-14s %-16s %-25s\n", "id","상호","분류","배달팁","최소 주문 금액", "전화번호", "주소");
                     while (rs.next())
                         System.out.printf("%-4d %-10s %-5s %-10d %-17d %-19s %-25s\n\n", rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getInt(5), rs.getString(6), rs.getString(8)+" "+rs.getString(9));
 
-                    rs = stmt.executeQuery("SELECT * FROM Menu where store = " + storeId + ";");
+                    rs = stmt.executeQuery("SELECT * FROM Menu where store = " + store_id + ";");
                     System.out.println("메뉴 목록");
                     System.out.printf("%-4s %-10s %-5s %-8s %-14s\n", "id","분류","이름","설명","가격");
                     while (rs.next())
                         System.out.printf("%-4d %-10s %-5s %-10s %-17d\n", rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getInt(5));
 
-                    rs = stmt.executeQuery("SELECT * FROM Review where order_id IN (SELECT id FROM Orders where store = " + storeId + ");");
+                    rs = stmt.executeQuery("SELECT * FROM Review where order_id IN (SELECT id FROM Orders where store = " + store_id + ");");
                     System.out.println("\n리뷰 목록");
                     System.out.printf("%-4s %-30s\n", "별점","리뷰");
                     while (rs.next())
                         System.out.printf("%-4d %-30s\n\n", rs.getInt(3), rs.getString(4));
 
-                    OrderAndReview orderAndReview = new OrderAndReview(userId, storeId, con);
+
+
+                    //OrderAndReview 생성
+
+
 
                 } catch (Exception e) {
                     System.out.println(e);
